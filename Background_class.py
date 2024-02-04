268b0235be12a9d0cdbcb954acb39d5ae6ef3ec0
Hubert Moncayo <hubert.moncayo@etu.enseeiht.fr>
2024-02-04 13:24:08 +0100

    background change on key

--- Background_class.py
+++ Background_class.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 from const import *
 
 class Background():
@@ -22,6 +23,20 @@ class Background():
             self.bgY1 = -self.rectBGimg.height
         if self.bgY2 >= self.rectBGimg.height:
             self.bgY2 = -self.rectBGimg.height
+
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_1]:
+            self.type = 0
+        elif pressed_keys[K_2]:
+            self.type = 1
+        elif pressed_keys[K_3]:
+            self.type = 2
+        elif pressed_keys[K_4]:
+            self.type = 3
+        elif pressed_keys[K_5]:
+            self.type = 4
+
+        self.bgX = -self.rectBGimg.width * self.type / 5
              
       def render(self, screen):
          screen.blit(self.bgimage, (self.bgX, self.bgY1))
