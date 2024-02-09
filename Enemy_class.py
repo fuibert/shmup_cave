617ce940bc0423817d94178b785126d72bcfacd1
locoste <louis.coste2@live.fr>
2024-02-09 20:40:28 +0100

    update enemy appartition
    Angle de vol & apparition généré aléatoirement

--- Enemy_class.py
+++ Enemy_class.py
@@ -13,10 +13,10 @@ class Enemy(pygame.sprite.Sprite):
         self.type = randint( 0, len(ENEMY_IMAGES) - 1)
         self.image_base = pygame.transform.rotate(
             pygame.image.load("textures/" + ENEMY_IMAGES[self.type]), 180.0)
-        
-        self.pos = pygame.math.Vector2(SCREEN_WIDTH / 2, 25)
+        self.start_position = randrange(0, SCREEN_WIDTH)
+        self.pos = pygame.math.Vector2(self.start_position, -10)
         self.speed = pygame.math.Vector2( 0, 200)
-        self.direction = randrange( -25000, 25000) / 1000
+        self.direction = self.start_angle()
         self.speed.rotate_ip( -self.direction)
         self.image = pygame.transform.rotate(self.image_base, self.direction)
 
@@ -27,6 +27,19 @@ class Enemy(pygame.sprite.Sprite):
 
         self.health = ENEMY_HEALTH * (self.type / 10)
         self.points = self.health
+
+    def start_angle(self):
+        # calcul de l'angle à gauche de l'avion
+        left_AC = self.start_position
+        left_AB = SCREEN_HEIGHT
+        left_degree = -round(int(math.degrees(math.atan(left_AC / left_AB))), 2)
+
+        # calcul de l'angle à droite de l'avion
+        rigth_AC = SCREEN_WIDTH - self.start_position
+        rigth_AB = SCREEN_HEIGHT
+        rigth_degree = round(int(math.degrees(math.atan(rigth_AC/ rigth_AB))), 2)
+
+        return randrange(left_degree,rigth_degree)
 
     def move(self):
         self.pos = self.pos + self.speed / FPS
