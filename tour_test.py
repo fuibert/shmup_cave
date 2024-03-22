5e722687fd8c0f25c62a984c86fbc6d1ea66f6a9
Hubert Moncayo <hubert.moncayo@gmail.com>
2024-03-22 10:23:09 +0100

    add tour test

---
+++ tour_test.py
@@ -0,0 +1,36 @@
+from gpiozero import LED, Button
+import time
+from  io_cave import *
+
+
+def old() :
+    led = LED(16)
+    #led = LED(23)
+    #led = LED(24)
+
+    try:
+        while True:
+            print("led")
+            led.on()
+            time.sleep(0.1)
+        
+    except KeyboardInterrupt:
+        pass
+    led.off()
+
+def new() :
+    if not IO_Controller.verre():
+        print("en attente d'un verre")
+    else:
+        print("letzgo")
+
+mode = "old"
+if mode == "new" :
+    IO_Controller = IO_Controller()
+    try:
+        while True :
+            new()        
+    except KeyboardInterrupt:
+        pass
+else :
+    old()
